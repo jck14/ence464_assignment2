484c626466f909dbf1fc11f9db2e39ea78d99a3d
unturnednz <hugo.king2@hotmail.com>
2017-10-11 15:59:27 +1300

    Stable-Still get -ve values

--- jacobianRelax/cmake-build-debug/CMakeFiles/jacobianRelax.dir/DependInfo.cmake
+++ jacobianRelax/cmake-build-debug/CMakeFiles/jacobianRelax.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jkjoka/CLionProjects/jacobianRelax/main.cpp" "/home/jkjoka/CLionProjects/jacobianRelax/cmake-build-debug/CMakeFiles/jacobianRelax.dir/main.cpp.o"
+  "/home/jkjoka/ence464/ence464_assignment2/jacobianRelax/main.cpp" "/home/jkjoka/ence464/ence464_assignment2/jacobianRelax/cmake-build-debug/CMakeFiles/jacobianRelax.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- jacobianRelax/main.cpp
+++ jacobianRelax/main.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
+#include <vector>
+
+class Array3D {
+    size_t m_width, m_height;
+    std::vector<float> m_data;
+public:
+    Array3D(size_t x, size_t y, size_t z, float init = 0):
+            m_width(x), m_height(y), m_data(x*y*z, init)
+    {}
+    float& operator()(size_t x, size_t y, size_t z) {
+        return m_data.at(x + y * m_width + z * m_width * m_height);
+    }
+};
 
 int main() {
 
-    const int MAX_ROW = 5;
-    const int MAX_COL = 5;
-    const int MAX_HEIGHT = 5;
+    const int N = 5;
+    float f = 0;
 
-    unsigned float dataArray[MAX_ROW][MAX_COL][MAX_HEIGHT]
+    Array3D dataArray(N, N, N, 0.0);
+    Array3D tempArray(N, N, N, 0.0);
+    dataArray(2,2,2) = 1.0;
 
-    for(int Row = 1; Row < MAX_ROW; ++Row) {
-        for (int Col = 1; Col < MAX_COL; ++Col) {
-            for (int Height = 1; Height < MAX_HEIGHT; ++Height) {
-                dataArray[Row][Col][Height] = 1/6 * (dataArray[Row+1][Col][Height] + dataArray[Row-1][Col][Height]+
-                        dataArray[Row][Col+1][Height] + dataArray[Row][Col-1][Height]+dataArray[Row][Col][Height+1] +
-                        dataArray[Row][Col][Height-1]);
-                printf("%f",dataArray[Row][Col][Height]);
+    printf("Jacobian Relaxation");
+
+    for(int i = 0; i < 50; i++) {
+        for (int Row = 1; Row < N - 1; Row++) {
+            for (int Col = 1; Col < N - 1; Col++) {
+                printf("\n----\n");
+                for (int Height = 1; Height < N - 1; Height++) {
+                    if (Row == 2 && Col == 2 && Height == 2) {
+                        f = 0.0;
+                    } else {
+                        f = 0.0;
+                    }
+                    tempArray(Row, Col, Height) =
+                            (dataArray(Row + 1, Col, Height) + dataArray(Row - 1, Col, Height) +
+                                               dataArray(Row, Col + 1, Height) + dataArray(Row, Col - 1, Height) +
+                                               dataArray(Row, Col, Height + 1) +
+                                               dataArray(Row, Col, Height - 1) - f)/6.0;
+                    printf("\n%f", dataArray(Row, Col, Height));
+                }
             }
         }
+        printf("-------------------------------\n\n");
+        dataArray = tempArray;
     }
 
-    return 0;
+    printf("\n\n");
+    return EXIT_SUCCESS;
 }
