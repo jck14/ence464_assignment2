7770de88772463d46ba68ce9f5435d3af847c52b
unturnednz <hugo.king2@hotmail.com>
2017-10-11 13:51:36 +1300

    unstable jacobian method

---
+++ jacobianRelax/cmake-build-debug/CMakeFiles/jacobianRelax.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jacobianRelax.dir/main.cpp.o"
+  "jacobianRelax.pdb"
+  "jacobianRelax"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jacobianRelax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
